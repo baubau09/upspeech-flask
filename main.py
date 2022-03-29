728f35a3aa864334c51cd5f7e30e86bbba733035
baubau09 <katietran.182@gmail.com>
2022-03-29 16:54:42 +0700

    import firebase stuff

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from firebase_admin import credentials, firestore, initialize_app, storage
+from google.cloud import storage
 
 app = Flask(__name__)
+
+# Initialize Firestore DB
+cred = credentials.Certificate('key.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+todo_ref = db.collection('todos')
 
 @app.route("/")
 def hello_world():
