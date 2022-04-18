e487c75197508a68fd077c62df0625ae09091272
baubau09 <katietran.182@gmail.com>
2022-04-18 09:24:45 +0700

    minor fix

--- validate.py
+++ validate.py
@@ -20,6 +20,6 @@ def validate_audio(url, filename):
         sound2 = sound2.set_frame_rate(44100)
         sound2 = sound2.set_channels(1)
         file_handle = sound2.export(filename, format="wav")
-    return 'The new sample rate is ' + str(sound2.frame_rate) + ', bit-depth is ' + str(sound2.sample_width*8) + ', channel is ' + str(sound2.channels)
+        return 'The new sample rate is ' + str(sound2.frame_rate) + ', bit-depth is ' + str(sound2.sample_width*8) + ', channel is ' + str(sound2.channels)
 
 # print(validate_audio("https://firebasestorage.googleapis.com/v0/b/upspeech-48370.appspot.com/o/test%2Funtitled5.wav?alt=media&token=3284d1a0-5db2-4d39-963d-bd216b3f48f6", "test_untitled5.wav"))
